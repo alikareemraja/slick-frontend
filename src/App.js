1abf30d7dde9dcb1bf6b08d38073de814f8a1418
AldoKacorri <kacorrialdo1996@gmail.com>
2019-07-19 17:39:02 +0200

    app js changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home'
 import Register from './Register';
+import { ItemListView } from './views/ItemListView';
+import { ItemDetailView }   from './views/ItemDetailView';
 
 
 export default class App extends Component {
